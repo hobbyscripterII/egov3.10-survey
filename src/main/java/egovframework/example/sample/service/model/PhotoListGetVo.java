67a863806d8ef84e637553bb7049e7a7fc1f6f14
이주영 <wruomma2@naver.com>
2024-05-06 15:09:54 +0900

    feat: 사진 게시판 목록 출력

--- src/main/java/egovframework/example/sample/service/model/PhotoListGetVo.java
+++ src/main/java/egovframework/example/sample/service/model/PhotoListGetVo.java
@@ -1,22 +1,17 @@
 package egovframework.example.sample.service.model;
 
 public class PhotoListGetVo {
-	private int ifile;
 	private int iboard;
-	private String originalName;
-	private String savedName;
-	private String ext;
+	private String title;
+	private String createdAt;
+	private int view;
 	
-	public int getIfile() { return ifile; }
-	public void setIfile(int ifile) { this.ifile = ifile; }
 	public int getIboard() { return iboard; }
 	public void setIboard(int iboard) { this.iboard = iboard; }
-	public String getOriginalName() { return originalName; }
-	public void setOriginalName(String originalName) { this.originalName = originalName; }
-	public String getSavedName() { return savedName; }
-	public void setSavedName(String savedName) { this.savedName = savedName; }
-	public String getExt() { return ext; }
-	public void setExt(String ext) { this.ext = ext; }
-	
-	@Override public String toString() { return "PhotoListGetVo [ifile=" + ifile + ", iboard=" + iboard + ", originalName=" + originalName + ", savedName=" + savedName + ", ext=" + ext + "]"; }
+	public String getTitle() { return title; }
+	public void setTitle(String title) { this.title = title; }
+	public String getCreatedAt() { return createdAt; }
+	public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
+	public int getView() { return view; }
+	public void setView(int view) { this.view = view; }
 }
--- src/main/webapp/WEB-INF/jsp/photo/list.jsp
+++ src/main/webapp/WEB-INF/jsp/photo/list.jsp
@@ -20,14 +20,13 @@ p { margin-bottom: 0 !important; }
 <div class="div-photo-board-list-wrap">
 	<c:forEach var="l" items="${list }">
 		<div class="div-img-thumbnail-wrap">
-			<a href="#"><img alt="썸네일" class="img-fluid img-thumbnail" src="/winitech/img/${l.savedName }${l.ext }"></a>
-			<p style="" id="p-img-thumbnail-title">제목</p>
-			<p style="color: #9E9E9E"><span>2024-05-03 </span><span>
+			<p style="" id="p-img-thumbnail-title"><c:out value="${l.title }" /></p>
+			<p style="color: #9E9E9E"><span><c:out value="${l.createdAt }" /></span><span>
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
 			  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
 			  <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
 			</svg>
-			0</span></p>
+			<c:out value="${l.view }" /></span></p>
 		</div>
 	</c:forEach>
 </div>
